a71411b5ff49296a6e58786a5a33ced09ea76335
My PC <mrphamdat099@gmail.com>
2019-11-28 10:13:35 +0700

    change pass

--- src/main/java/com/hotel/demo/controller/TestRestAPIs.java
+++ src/main/java/com/hotel/demo/controller/TestRestAPIs.java
@@ -10,8 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestRestAPIs {
 
     @GetMapping("/api/test/user")
-    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
-    public String userAccess()
+  public String userAccess()
     {
         return ">>> User Contents!";
     }
